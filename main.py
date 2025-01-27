6c05d495a5c503bd1425d82af9c65cae3f050b6e
enzo2346 <94357357+enzo2346@users.noreply.github.com>
2025-01-27 22:34:18 +0100

    Clean up code

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pytesseract # pytesseract is a wrapper for Google's Tesseract-OCR Engine: https://github.com/UB-Mannheim/tesseract/wiki
-from PIL import Image, ImageEnhance, ImageFilter
 from pdf2image import convert_from_path # pdf2image is a wrapper for poppler, a PDF rendering library: https://github.com/oschwartz10612/poppler-windows/releases
 
 file_path = r'file2convert.pdf'
@@ -14,6 +13,6 @@ ocr_text_by_page = [pytesseract.image_to_string(image, lang="fra") for image in 
 # Combine OCR results into a single text block
 ocr_full_text = "\n".join(ocr_text_by_page)
 
-# Write OCR results to a text file with UTF-8 encoding to correctly handle special characters
+# Write OCR results to a text file
 with open(output_path, 'w', encoding='utf-8') as file:
     file.write(ocr_full_text)
